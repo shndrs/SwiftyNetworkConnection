207494f9b10894fefc3600f8ad508024817bf6ad
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-08-01 10:42:58 +0430

    first commit

---
+++ SwiftyNetworkConnection/SwiftyNetworkConnection.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyNetworkConnection.h
+//  SwiftyNetworkConnection
+//
+//  Created by shndrs on 8/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyNetworkConnection.
+FOUNDATION_EXPORT double SwiftyNetworkConnectionVersionNumber;
+
+//! Project version string for SwiftyNetworkConnection.
+FOUNDATION_EXPORT const unsigned char SwiftyNetworkConnectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyNetworkConnection/PublicHeader.h>
+
+
