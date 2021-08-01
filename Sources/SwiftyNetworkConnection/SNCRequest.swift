5d4a8ba8aac29093f741668675d990bd7e2521d5
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-08-01 13:13:54 +0430

    Updated SNCRequest with thread handling blocks

--- Sources/SwiftyNetworkConnection/SNCRequest.swift
+++ Sources/SwiftyNetworkConnection/SNCRequest.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class SNCRequest: NSObject {
+open class SNCRequest: NSObject {
     
     private let url: URL
     
@@ -16,30 +16,44 @@ public class SNCRequest: NSObject {
         self.url = baseUrl
     }
     
-    public func connect(body: SNCBody,
-                        httpMethod: SNCHttpMethods = .get ,
+}
+
+// MARK: - Methods
+
+extension SNCRequest {
+    
+    public func connect(body: SNCBody? = nil,
+                        httpMethod: SNCHttpMethods = .get,
                         callback: @escaping(Result<SNCResponse, APIError>) -> Void) {
-        do {
-            var request = URLRequest(url: url)
-            request.httpMethod = httpMethod.rawValue
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpBody = try JSONEncoder().encode(body)
-            let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let resp = response as? HTTPURLResponse, resp.statusCode == 200,
-                      let jsonData = data else {
-                    callback(.failure(.reponseError))
-                    return
-                }
-                do {
-                    let data = try JSONDecoder().decode(SNCResponse.self, from: jsonData)
-                    callback(.success(data))
-                } catch {
-                    callback(.failure(.decodingError))
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let self = self else { return }
+            do {
+                var request = URLRequest(url: self.url)
+                request.httpMethod = httpMethod.rawValue
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpBody = try JSONEncoder().encode(body)
+                URLSession.shared.dataTask(with: request) { data, response, error in
+                    guard let resp = response as? HTTPURLResponse, resp.statusCode == 200,
+                          let jsonData = data else {
+                        callback(.failure(.reponseError))
+                        return
+                    }
+                    do {
+                        let data = try JSONDecoder().decode(SNCResponse.self, from: jsonData)
+                        DispatchQueue.main.async {
+                            callback(.success(data))
+                        }
+                    } catch {
+                        DispatchQueue.main.async {
+                            callback(.failure(.decodingError))
+                        }
+                    }
+                }.resume()
+            } catch {
+                DispatchQueue.main.async {
+                    callback(.failure(.encodingError))
                 }
             }
-            dataTask.resume()
-        } catch {
-            callback(.failure(.encodingError))
         }
     }
     
